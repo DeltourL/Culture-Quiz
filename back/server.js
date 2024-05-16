781557d315e0ba98e9312d0f3fe7a8f85dcf10f6
Deltourl <lucas.deltour95@gmail.com>
2024-05-16 12:36:06 +0200

    API routes for categories and questions

--- back/server.js
+++ back/server.js
@@ -1,11 +1,51 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/culture_quiz', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connexion à MongoDB réussie'))
+    .catch(err => console.error('Erreur de connexion à MongoDB :',
+        err));
+
+const questionSchema = new mongoose.Schema({
+    text: { type: String, required: true },
+    category: { type: String, required: true },
+    correctAnswer: { type: String, required: true },
+    wrongAnswers: [{ type: String, required: true }]
+});
+const Question = mongoose.model('Question', questionSchema, "questions");
+module.exports = Question;
+
+
+app.get('/categories', async (req, res) => {
+    try {
+        const categories = await Question.distinct("category");
+
+        console.log(categories);
+        res.send(categories);
+    } catch (err) {
+        console.error('Error finding categories :', err);
+        res.status(500).send('Error finding categories');
+    }
 });
 
+app.get('/questions/:category', async (req, res) => {
+    try {
+        const questions = await Question.find({category : req.params.category});
+
+        console.log(questions);
+        res.send(questions);
+    } catch (err) {
+        console.error('Error finding questions :', err);
+        res.status(500).send('Error finding questions');
+    }
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
