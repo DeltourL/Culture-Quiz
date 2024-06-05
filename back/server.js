1b345f7afa1d383c3ab5096c1c92a888c0c25138
Deltourl <lucas.deltour95@gmail.com>
2024-06-05 12:02:16 +0200

    added cors to server

--- back/server.js
+++ back/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
+
 
 mongoose.connect('mongodb://localhost:27017/culture_quiz', {
     useNewUrlParser: true,
