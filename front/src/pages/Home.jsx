83d937c46a93be0eaa440b6c302bdbe55953decb
Deltourl <lucas.deltour95@gmail.com>
2024-06-12 16:46:09 +0200

    added logo

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,13 +1,14 @@
-// TODO logo, styles
+// TODO styles
 
 import React from 'react';
 import Category from '../components/Category';
+import logo from '../logo.png';
 
 export default function Home() {
 
     return (
         <div className='Body'>
-            <h1>LOGO</h1>
+            <img src={logo} alt="logo" />
             <h1>Culture Quiz</h1>
             <Category />
         </div>
